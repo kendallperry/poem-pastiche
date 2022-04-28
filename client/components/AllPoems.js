686947e95905890d86c2545c1b461a7bcd1fb658
Kendall Perry <perry.kendall@gmail.com>
2022-04-28 17:46:12 -0400

    trying to fix history.push, page working otherwise

--- client/components/AllPoems.js
+++ client/components/AllPoems.js
@@ -4,52 +4,66 @@ import { fetchPoems } from "../store/poems";
 import AddLine from "./AddLine";
 
 class AllPoems extends React.Component {
-    constructor(props) {
-      super(props);
-    }
+  constructor(props) {
+    super(props);
+  }
 
-    componentDidMount() {
-        this.props.displayPoems();
-    }
+  componentDidMount() {
+    this.props.displayPoems();
+  }
 
-    render() {
-        const { poems } = this.props;
-        return (
-            <div id="all-poems-container">
-                <ul id="all-poems">
-                    {poems.length === 0 ? <h2>No Poems Yet!</h2> : null}
-                    {poems.map((poem) => {
-                        return (
-                            <div className="poem" key={poem.id}>
-                                <strong><h4>{poem.title}</h4></strong><br />
-                                <p>{poem.firstLine}</p>
-                                <p>{poem.lines[0] ? poem.lines[0].line : null}</p>
-                                <p>{poem.lines[1] ? poem.lines[1].line : null}</p>
-                                <p>{poem.lines[2] ? poem.lines[2].line : null}</p>
-                                <p>{poem.lines[3] ? poem.lines[3].line : null}</p>
-                                <br /><br /><br />
-                                <AddLine poem={poem} />
-                                {poem.user ? <i><p>started by: {poem.user.username}</p></i> : null}
+  //   componentDidUpdate() {
+  //       if (this.props.poems.lines.length !== this.props.poems.lines.length) {
+  //         this.props.displayPoems();
+  //       }
+  //   }
 
-                            </div>
-                        )
-                    })}
-                </ul>
-            </div>
-        )
-    }
+  render() {
+    const { poems } = this.props;
+    return (
+      <div id="all-poems-container">
+        <ul id="all-poems">
+          {poems.length === 0 ? <h2>No Poems Yet!</h2> : null}
+          {poems.map((poem) => {
+            return (
+              <div className="poem" key={poem.id}>
+                <strong>
+                  <h4>{poem.title}</h4>
+                </strong>
+                <br />
+                <p>{poem.firstLine}</p>
+                <p>{poem.lines[0] ? poem.lines[0].line : null}</p>
+                <p>{poem.lines[1] ? poem.lines[1].line : null}</p>
+                <p>{poem.lines[2] ? poem.lines[2].line : null}</p>
+                <p>{poem.lines[3] ? poem.lines[3].line : null}</p>
+                <br />
+                <br />
+                <br />
+                <AddLine poem={poem} />
+                {poem.user ? (
+                  <i>
+                    <p>started by: {poem.user.username}</p>
+                  </i>
+                ) : null}
+              </div>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  }
 }
 
 const mapState = (state) => {
-    return {
-        poems: state.poems,
-    }
-}
+  return {
+    poems: state.poems,
+  };
+};
 
 const mapDispatch = (dispatch) => {
-    return {
-        displayPoems: () => dispatch(fetchPoems()),
-    }
-}
+  return {
+    displayPoems: () => dispatch(fetchPoems()),
+  };
+};
 
 export default connect(mapState, mapDispatch)(AllPoems);
